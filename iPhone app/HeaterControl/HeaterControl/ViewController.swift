a4dd6ddb39038541c354ea9f75062f1568d6b21f
Bradley Clayton <bclayton@deloitte.com.au>
2016-10-13 10:52:53 +1100

    Cleaned up the UI

--- iPhone app/HeaterControl/HeaterControl/ViewController.swift
+++ iPhone app/HeaterControl/HeaterControl/ViewController.swift
@@ -12,30 +12,31 @@ import Alamofire
 class ViewController: UIViewController {
 
     @IBOutlet weak var tempLabel: UILabel!
-    @IBOutlet weak var humidLabel: UILabel!
-    
+    @IBOutlet weak var loadingView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        loadingView.isHidden = true
     }
 
     @IBAction func refreshButtonPressed(_ sender: UIButton) {
+        loadingView.isHidden = false
         Alamofire.request("https://api.particle.io/v1/devices/510028000b51343334363138/temp?access_token=d2d8eab7d7cecfbc33b225c620b939b371b0ed9e").responseJSON { response in
 
-        
+            self.loadingView.isHidden = true
             if (response.result.isSuccess) {
                 if let json = response.result.value {
                     debugPrint("JSON: \(json)")
+                    
                     let info = json as! NSDictionary
                     if let temp = info.value(forKey: "result") as? String {
-                        debugPrint(temp);
-                        self.tempLabel.text = temp + "° C";
+                        debugPrint(temp)
+                        if let doubleTemp = Double(temp) {
+                            self.tempLabel.text = String(format:"%.1f°C", doubleTemp);
+                        }
                     }
+                    
                 }
-                
-                
             }
-            
             
         }
     }
